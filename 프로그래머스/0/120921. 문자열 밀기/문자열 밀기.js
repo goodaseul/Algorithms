527e9514618fc35b76f909a771b30b5bf4709c87
Jeong Daseul <98886223+goodaseul@users.noreply.github.com>
2025-04-25 14:15:56 +0900

    [level 0] Title: 문자열 밀기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120921. 문자열 밀기/문자열 밀기.js
@@ -0,0 +1,8 @@
+function solution(A, B) {
+    for(let i = 0; i < A.length; i ++){
+        if( A === B) return i;
+        A = A[A.length -1] + A.slice(0, A.length -1)
+    }
+    
+    return -1
+}
